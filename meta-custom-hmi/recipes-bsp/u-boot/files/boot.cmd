829c856a4da4504012cf2915ad258a5aaa2205de
Bhargav <bhargav@example.com>
2026-09-07 17:35:23 +0530

    display: hide linux console cursor on fb0 via vt.global_cursor_default=0 and ANSI escape

--- meta-custom-hmi/recipes-bsp/u-boot/files/boot.cmd
+++ meta-custom-hmi/recipes-bsp/u-boot/files/boot.cmd
@@ -1,7 +1,7 @@
 echo "=== Loading MarknStamp HMI Linux System ==="
 setenv mmcdev 0
 setenv mmcroot '/dev/mmcblk0p2 rootwait rw'
-setenv bootargs console=ttymxc0,115200 root=${mmcroot} quiet
+setenv bootargs console=ttymxc0,115200 root=${mmcroot} quiet vt.global_cursor_default=0
 load mmc 0:1 ${loadaddr} zImage
 if load mmc 0:1 ${fdt_addr} okmx6ull-c-emmc.dtb; then
     bootz ${loadaddr} - ${fdt_addr}
