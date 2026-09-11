c87a0212cd6c32fc26413f439e7e081d692922ef
Bhargav <bhargav@example.com>
2026-09-11 18:42:11 +0530

    Suppress tty1 login prompt and blinking cursor on LCD display

--- meta-custom-hmi/recipes-bsp/u-boot/files/boot.cmd
+++ meta-custom-hmi/recipes-bsp/u-boot/files/boot.cmd
@@ -1,7 +1,7 @@
 echo "=== Loading MarknStamp HMI Linux System ==="
 setenv mmcdev 0
 setenv mmcroot '/dev/mmcblk0p2 rootwait rw'
-setenv bootargs console=ttymxc0,115200 root=${mmcroot} quiet
+setenv bootargs console=ttymxc0,115200 root=${mmcroot} quiet loglevel=0 vt.global_cursor_default=0 systemd.mask=getty@tty1.service
 setenv splashimage 0x89000000
 setenv splashpos m,m
 
