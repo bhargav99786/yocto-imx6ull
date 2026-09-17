aa244ca3f96cd896b1517fe88e3779ccfd456637
Bhargav <bhargav@example.com>
2026-09-17 11:11:04 +0530

    fix(boot): pass logo.nologo to suppress kernel logo flicker and avoid duplicate splash blit

--- meta-custom-hmi/recipes-bsp/u-boot/files/boot.cmd
+++ meta-custom-hmi/recipes-bsp/u-boot/files/boot.cmd
@@ -11,15 +11,16 @@ else
     setenv mmcroot '/dev/mmcblk0p2 rootwait rw'
 fi
 
-setenv bootargs console=ttymxc0,115200 root=${mmcroot} quiet loglevel=0 vt.global_cursor_default=0 systemd.mask=getty@tty1.service
+# Suppress console flicker, disable kernel PPM logo (logo.nologo), hide VT cursor
+setenv bootargs console=ttymxc0,115200 root=${mmcroot} quiet loglevel=0 vt.global_cursor_default=0 logo.nologo systemd.mask=getty@tty1.service
 setenv splashimage 0x83800000
 setenv splashpos m,m
 
-# Load user-replaceable BMP splash image from FAT boot partition
-if load mmc ${mmcdev}:1 ${splashimage} splash.bmp; then
-    bmp display ${splashimage}
-elif load mmc ${mmcdev}:1 ${splashimage} logo.bmp; then
-    bmp display ${splashimage}
+# Only load splash.bmp if logo was not already rendered by U-Boot video init
+if test -z "${videodone}"; then
+    if load mmc ${mmcdev}:1 ${splashimage} splash.bmp; then
+        bmp display ${splashimage}
+    fi
 fi
 
 load mmc ${mmcdev}:1 ${loadaddr} zImage
